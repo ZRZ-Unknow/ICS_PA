1e240d8519ca1f945582604aa423ba4670029db3
tracer-ics2019 <tracer@njuics.org>
2019-09-14 20:20:15 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:20:15 up 44 min,  1 user,  load average: 0.12, 0.21, 0.32 5b80040d6d30941dbef15ca9a5a1c49081d9a911

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -90,8 +90,8 @@ static int cmd_x(char *args){
   paddr_t addr;
   sscanf(expr,"%x",&addr);
   for (int i=0;i<num;i++){
-     int memory=paddr_read(addr+i*4,4);
-     printf("0x%08x: ",addr+i*4);
+     int memory=paddr_read(addr+i*16,4);
+     printf("0x%08x: ",addr+i*16);
      for (int j=0;j<4;j++){
         printf("%d ",memory);
 	memory=memory>>8;
