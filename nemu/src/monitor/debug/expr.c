345258a6b6986b1104988b81d8fb7f7bf41c0a37
tracer-ics2019 <tracer@njuics.org>
2019-09-21 03:18:48 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  03:18:48 up  4:06,  1 user,  load average: 0.11, 0.21, 0.23 f6c01151978fc8251b6d3bbcf98efbbd5761985f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -213,7 +213,7 @@ uint32_t eval(int p,int q){
   else if (p==q) {uint32_t num=0;sscanf(tokens[p].str,"%d",&num);return num;}
   else if (check_parentheses(p,q)) {return eval(p+1,q-1);}
   else {
-    int op=main_operator(p,q);printf("eval %s,%d\n",tokens[op].str,tokens[op].type);
+    int op=main_operator(p,q);printf("eval %s,%d\n",tokens[op].str,op);
     uint32_t val1=eval(p,op-1);
     uint32_t val2=eval(op+1,q);
     switch(tokens[op].type){
