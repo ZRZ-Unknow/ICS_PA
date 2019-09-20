d7bab8ebb79f145b10851e4b166820c4ff46091f
tracer-ics2019 <tracer@njuics.org>
2019-09-21 00:47:23 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:47:23 up  1:35,  1 user,  load average: 0.24, 0.28, 0.27 f17e5fcb13ebc95abf7b7fef02876ba3d224eb7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -217,7 +217,7 @@ int eval(int p,int q){
       case '-':return val1-val2;
       case '*':return val1*val2;
       case '/':return val1/val2;
-      default:assert(0);	       
+      default:{printf("%d",tokens[op].type);assert(0);}	       
     }
   }
 }
