60582b8a518e77591683ecbbd536040cddbbbb16
tracer-ics2019 <tracer@njuics.org>
2019-10-24 21:55:29 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:55:29 up  1:46,  1 user,  load average: 0.86, 0.56, 0.50 12fb9dce46a1de1a5421113e1c2096f6c8b35c

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -9,11 +9,11 @@ make_EHelper(test) {
 
 make_EHelper(and) {
   rtl_and(&s0,&id_dest->val,&id_src->val);
-  s1=0;
-  rtl_set_OF(&s1);
-  rtl_set_CF(&s1);
-  rtl_update_ZFSF(&s0,id_dest->width);
   operand_write(id_dest,&s0);
+  rtl_update_ZFSF(&s0,id_dest->width);
+  s0=0;
+  rtl_set_OF(&s0);
+  rtl_set_CF(&s0);
 
   print_asm_template2(and);
 }
