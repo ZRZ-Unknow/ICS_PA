417308e9a049a435228863efa6403539c5a737c1
tracer-ics2019 <tracer@njuics.org>
2019-10-24 21:41:48 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:41:48 up  1:32,  1 user,  load average: 0.62, 0.52, 0.50 79fe9de4d8ac109418f5f738d5df3c08a469ca36

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -1,15 +1,29 @@
 #include "cpu/exec.h"
 
-make_EHelper(mov);
-make_EHelper(call);
-make_EHelper(push);
+
+//arith.c
 make_EHelper(sub);
-make_EHelper(xor);
+make_EHelper(inv);
+
+//cc.c
+
+//control.c
+make_EHelper(call);
 make_EHelper(ret);
 make_EHelper(jmp);
+
+//data-mov.c
+make_EHelper(mov);
+make_EHelper(push);
 make_EHelper(pop);
-make_EHelper(operand_size);
 make_EHelper(lea);
-make_EHelper(inv);
+//exec.c
+
+//logic.c
+make_EHelper(xor);
 make_EHelper(and);
+//prefix.c
+make_EHelper(operand_size);
+//special.c
 make_EHelper(nemu_trap);
+//system.c
--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -9,11 +9,11 @@ make_EHelper(test) {
 
 make_EHelper(and) {
   rtl_and(&s0,&id_dest->val,&id_src->val);
-  operand_write(id_dest,&s0);
+  s1=0;
+  rtl_set_OF(&s1);
+  rtl_set_CF(&s1);
   rtl_update_ZFSF(&s0,id_dest->width);
-  s0=0;
-  rtl_set_CF(&s0);
-  rtl_set_OF(&s0);
+  operand_write(id_dest,&s0);
 
   print_asm_template2(and);
 }
