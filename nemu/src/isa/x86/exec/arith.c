e22c60a46de719af8814ca34eb38582532123567
tracer-ics2019 <tracer@njuics.org>
2019-10-26 01:23:00 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:23:00 up 10 min,  1 user,  load average: 0.43, 0.25, 0.13 ed36647efbeaaf25fd3e0a962f0601d6faf716a1

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -11,20 +11,20 @@ make_EHelper(add) {
   operand_write(id_dest, &s0);
 
   if (id_dest->width != 4) {
-    rtl_andi(&s1, &s1, 0xffffffffu >> ((4 - id_dest->width) * 8));
+    rtl_andi(&s0, &s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
   }
 
   rtl_update_ZFSF(&s0, id_dest->width);
 
   // update CF
   //rtl_is_add_carry(&s1, &s1, &s0);
-  rtl_is_add_carry(&s0, &s0, &id_dest->val);
+  rtl_is_add_carry(&s1, &s0, &id_dest->val);
   //rtl_or(&s0, &s0, &s1);
-  rtl_set_CF(&s0);
+  rtl_set_CF(&s1);
 
   // update OF
-  rtl_is_add_overflow(&s0, &s0, &id_dest->val, &id_src->val, id_dest->width);
-  rtl_set_OF(&s0);
+  rtl_is_add_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
+  rtl_set_OF(&s1);
 
   print_asm_template2(add);
 }
