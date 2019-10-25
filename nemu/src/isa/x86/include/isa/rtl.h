68b79e6aa693132092a08478ef45ae8a8437d1e3
tracer-ics2019 <tracer@njuics.org>
2019-10-26 01:36:35 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:36:35 up 24 min,  1 user,  load average: 0.32, 0.30, 0.21 4ccf05bd3ace9a0b79ab739b62f4d724224cd2

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -96,7 +96,7 @@ make_rtl_setget_eflags(SF)
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
   //TODO();
-  cpu.eflags.ZF=((*result & (0xFFFFFFFF >>((4-width)*8)))==0);
+  cpu.eflags.ZF=((*result & (0xFFFFFFFF >>((4-width)*8)))!=0);
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
