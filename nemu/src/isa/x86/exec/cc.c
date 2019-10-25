528008d4ea051e52308eded0cc6cb875790e217e
tracer-ics2019 <tracer@njuics.org>
2019-10-26 00:46:31 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:46:31 up  8:28,  1 user,  load average: 1.27, 0.84, 0.59 f58d3b3841c4d4a74f35f7a0c715782d3ba875

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -17,10 +17,10 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_O:rtl_get_OF(dest);break;
     case CC_B:rtl_get_CF(dest);break;
     case CC_E:rtl_get_ZF(dest);break;
-    case CC_BE:*dest=(cpu.eflags.CF||cpu.eflags.ZF);break;
+    case CC_BE:*dest=((cpu.eflags.CF)||(cpu.eflags.ZF));break;
     case CC_S:rtl_get_SF(dest);break;
     case CC_L:*dest=(cpu.eflags.SF!=cpu.eflags.OF);break;
-    case CC_LE:*dest=(cpu.eflags.ZF ||(cpu.eflags.SF!=cpu.eflags.OF));break;
+    case CC_LE:*dest=((cpu.eflags.ZF) ||(cpu.eflags.SF!=cpu.eflags.OF));break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
