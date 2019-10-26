c6790840ffc14df4a823dec98f36a3097e0b36ba
tracer-ics2019 <tracer@njuics.org>
2019-10-27 00:26:04 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:26:04 up  1:41,  1 user,  load average: 0.39, 0.54, 0.44 edd1788e6332902ef02141aecbc0e9bf184a168

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -12,6 +12,14 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
      ref_r->edi!=cpu.edi||
      ref_r->pc!=pc
     ){
+      printf("qemu:eax is 0x%08x,nemu eax is 0x%08x, at pc:0x%08x\n",ref_r->eax,cpu.eax,pc);
+      printf("qemu:ecx is 0x%08x,nemu ecx is 0x%08x, at pc:0x%08x\n",ref_r->ecx,cpu.ecx,pc);
+printf("qemu:edx is 0x%08x,nemu edx is 0x%08x, at pc:0x%08x\n",ref_r->edx,cpu.edx,pc);
+printf("qemu:ebx is 0x%08x,nemu ebx is 0x%08x, at pc:0x%08x\n",ref_r->ebx,cpu.ebx,pc);
+printf("qemu:esp is 0x%08x,nemu esp is 0x%08x, at pc:0x%08x\n",ref_r->esp,cpu.esp,pc);
+printf("qemu:ebp is 0x%08x,nemu ebp is 0x%08x, at pc:0x%08x\n",ref_r->ebp,cpu.ebp,pc);
+printf("qemu:esi is 0x%08x,nemu esi is 0x%08x, at pc:0x%08x\n",ref_r->esi,cpu.esi,pc);
+printf("qemu:edi is 0x%08x,nemu edi is 0x%08x, at pc:0x%08x\n",ref_r->edi,cpu.edi,pc);
     return false;
   }
   return true;
