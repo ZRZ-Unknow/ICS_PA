588c503c9cc77dcc70694f8b134e9bff73164d7e
tracer-ics2019 <tracer@njuics.org>
2019-10-27 15:40:12 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  15:40:12 up  1:13,  1 user,  load average: 0.11, 0.15, 0.25 2bcc103aa9be86975a4d9b06ff2285c09d71c8b1

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,26 +3,66 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t len=0;
+  while((*s++)!='\0'){
+    len++;
+  }
+  return len;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  return strncpy(dst,src,strlen(src));
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  if(n>strlen(src)){
+    n=strlen(src);
+  }
+  char *res=dst;
+  while(n){
+    if((*res=*src)!=0){
+      src++;
+    }
+    res++;
+    n--;
+  }
+  return res;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char *temp=dst;
+  while(*dst){
+    dst++;
+  }
+  while((*dst++=*src++)!='\0');
+  return temp; 
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  int t=0;
+  while((t=(*s1-*s2))==0 && *s1 && *s2){
+    s1++;s2++;
+  }
+  if(t>0){
+    return 1;
+  }
+  else if(t<0){
+    return -1;
+  }
+  else{
+    return 0;
+  }
+  
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  assert(s1!=NULL && s2!=NULL);
+  while(n--){
+    if(*s1==0 ||*s1!=*s2){
+      return *s1-*s2;
+    }
+    s1++;s2++;
+  }
   return 0;
 }
 
