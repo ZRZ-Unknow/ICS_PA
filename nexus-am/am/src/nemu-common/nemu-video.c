4fa61ee5320ec0fccc3dd0c4b57d1922df6d995f
tracer-ics2019 <tracer@njuics.org>
2019-11-01 23:08:13 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:08:13 up  1:09,  1 user,  load average: 0.43, 0.42, 0.45 43ac805d48a2ef228e332f17ae6c5d1d8fb84fcd

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -3,6 +3,8 @@
 #include <nemu.h>
 #include <klib.h>
 #define SCREEN_PORT 0x100
+static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
+
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
@@ -20,7 +22,11 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-
+      int x=ctl->x,y=ctl->y,w=ctl->w,h=ctl->h;
+      uint32_t *pixels=ctl->pixels;
+      for(int i=0;i<h;i++){
+        memcpy(fb+(y+i)*screen_width()+x,pixels+i*w,w*4);
+      }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
