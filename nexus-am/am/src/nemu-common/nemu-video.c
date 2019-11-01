49b4c38297b05ad7a96039bc32f32fbee2012db4
tracer-ics2019 <tracer@njuics.org>
2019-11-02 00:29:18 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:29:18 up  2:31,  1 user,  load average: 0.32, 0.66, 0.69 b2a8f6891088ef96b26e21406c5b0dcdb89da069

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -3,7 +3,7 @@
 #include <nemu.h>
 #include <klib.h>
 #define SCREEN_PORT 0x100
-static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
+//static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
@@ -22,13 +22,17 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-      int x=ctl->x,y=ctl->y,w=ctl->w,h=ctl->h;
-      uint32_t *pixels=ctl->pixels;
-      int W=screen_width(),H=screen_height();
-      int cp_bytes =sizeof(uint32_t)*(w<W-x?w:W-x);
-      for(int i=0;i<h&&y+i<H;i++){
-        memcpy(&fb[(y+i)*W+x],pixels,cp_bytes);
-        pixels+=w;
+      //int x=ctl->x,y=ctl->y,w=ctl->w,h=ctl->h;
+      //uint32_t *pixels=ctl->pixels;
+      //int W=screen_width(),H=screen_height();
+      //int cp_bytes =sizeof(uint32_t)*(w<W-x?w:W-x);
+      //for(int i=0;i<h&&y+i<H;i++){
+      //  memcpy(&fb[(y+i)*W+x],pixels,cp_bytes);
+      //  pixels+=w;
+      //}
+      uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
+      for(int i=0;i<ctl->h;i++){
+        memcpy(&fb[(i+ctl->y)*screen_width()+ctl->x],&ctl->pixels[i*ctl->w],ctl->w*4);
       }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
