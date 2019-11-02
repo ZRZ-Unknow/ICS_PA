e1dab3da4b399d635a88c873440fb9f50633516d
tracer-ics2019 <tracer@njuics.org>
2019-11-03 01:09:43 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:09:43 up  1:45,  1 user,  load average: 0.43, 0.44, 0.35 ea4ca187c3fa17d47e34b71352aca482345f27cc

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -130,12 +130,12 @@ void interpret_rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret);
 
 /* RTL pseudo instructions */
 
- inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
+static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
   *dest=~(*src1);
 }
 
- inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   int32_t t=(int32_t)*src1;
   switch(width){
