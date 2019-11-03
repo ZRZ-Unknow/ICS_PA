6d3000acb76d32a2f9e8146ea09d1bdb7b97ce31
tracer-ics2019 <tracer@njuics.org>
2019-11-03 15:52:00 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  15:52:00 up  2:27,  1 user,  load average: 0.60, 0.57, 0.58 6db0d1c03bff8f29f6def3a1cff686466fca67d9

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -34,11 +34,8 @@ static void vga_io_handler(uint32_t offset, int len, bool is_write) {
   if(offset==0){
     assert(!is_write);
   }
-  else if(offset==4){
+  else {
     if(is_write){update_screen();}
-  }
-  else{
-    assert(0);
   }
 }
 
