af91aa4a585aef8770b94177c3388280b5a64856
181220076-Zhou Renzhe <1213714744@qq.com>
2019-11-03 16:43:52 +0800

    fix some bugs and finish pa2

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -48,12 +48,6 @@ make_EHelper(or) {
 make_EHelper(sar) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
-  /*if(id_dest->width==1){
-    id_dest->val=(int8_t)id_dest->val;
-  }
-  else if(id_dest->width==2){
-    id_dest->val=(int16_t)id_dest->val;
-  }*/
   rtl_sar(&s0,&id_dest->val,&id_src->val);
   operand_write(id_dest,&s0);
   rtl_update_ZFSF(&s0,id_dest->width);
