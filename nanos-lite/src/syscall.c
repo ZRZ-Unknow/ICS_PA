ee9825fa643738f87ca4ea930d9f9ff7644bdc1a
tracer-ics2019 <tracer@njuics.org>
2019-11-23 00:10:23 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:10:23 up  1:42,  1 user,  load average: 0.28, 0.48, 0.67 6d17d035175c9d775cce1dcbf9f48c232f8b590a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,7 +5,7 @@
 static inline int32_t sys_write(int fd,const void *buf,size_t len){
   if(fd==1||fd==2){
     char *b=(char*)buf;
-    printf("--%d\n",len);
+    //printf("--%d\n",len);
     for(int i=0;i<len;i++){
       //printf("----%s\n",b[i]);
       _putc(*(b++));
@@ -24,7 +24,7 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     case SYS_yield:_yield();c->GPRx=0;break;
     case SYS_exit:_halt(c->GPR2);break;
-    case SYS_write:c->GPRx=sys_write(a[1],(void*)a[2],a[3]);break;
+    case SYS_write:printf(";;;%d\n",a[3]);c->GPRx=sys_write(a[1],(void*)a[2],a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
