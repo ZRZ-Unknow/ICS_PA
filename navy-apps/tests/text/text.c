738222f8281095cfb345b92d01f8b1645a03475c
tracer-ics2019 <tracer@njuics.org>
2019-11-23 18:47:31 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  18:47:31 up  5:14,  1 user,  load average: 1.64, 0.94, 0.65 6223bc278814f20fc0a8d93ae28cda553f8c4a9e

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -31,7 +31,7 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }*/
-  fseek(fp, 2499, SEEK_SET);
+  fseek(fp, 2495, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     //printf("i is %d,n is %d\n",i,n);
     fscanf(fp, "%d", &n);
