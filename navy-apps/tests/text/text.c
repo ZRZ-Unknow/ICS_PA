9092f77ae0cd1b589d1db3f19db661743bdf0548
tracer-ics2019 <tracer@njuics.org>
2019-11-23 17:28:36 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:28:36 up  3:55,  1 user,  load average: 0.70, 0.94, 0.94 18e504b975fd347be68dc1ec7e16c4924ca2d719

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -11,7 +11,7 @@ int main() {
   fseek(fp, 0, SEEK_SET);//5*500
   int i, n;
   for (i = 500; i < 1000; i ++) {
-printf("curr pos is %d\n",ftell(fp));
+printf("currt pos is %d\n",ftell(fp));
     //printf("pos is %d\n",ftell(fp));
     fscanf(fp, "%d", &n);//没读一次前进5
     //assert(n == i + 1);
