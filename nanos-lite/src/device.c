15d4573ceaabf1ff57f4b725f3a0b0d4257001ef
tracer-ics2019 <tracer@njuics.org>
2019-11-23 14:11:40 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:11:40 up 38 min,  1 user,  load average: 1.16, 0.90, 0.81 55374c3f934df2b7a74a47e1ac4793e2ad0d2e22

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -3,6 +3,7 @@
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   //char *b=(char*)buf;
+  printf("off is %d,len is %d\n",offset,len);
   for(int i=offset;i<len;i++){
     _putc(((char*)buf)[i]);
   }
