53e090993395339a206eaa9360786126391e10c9
tracer-ics2019 <tracer@njuics.org>
2019-11-23 14:31:27 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:31:27 up 58 min,  1 user,  load average: 1.33, 0.96, 0.89 bad0c212fd376d903a4b243613ec06594421f66b

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -8,16 +8,19 @@ int main() {
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
+  printf("cc\n");
   assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
+  printf("aa\n");
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
+  printf("bb\n");
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
