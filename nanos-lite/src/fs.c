de97d693519c9d18897a77284b6b483ad1f4ba26
tracer-ics2019 <tracer@njuics.org>
2019-11-26 02:42:20 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  02:42:20 up  1:01,  1 user,  load average: 0.46, 0.46, 0.49 fb48ef9bfc47124ef7687c8ce21001ae1cb0cad2

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -38,7 +38,7 @@ static Finfo file_table[] __attribute__((used)) = {
 #include "files.h"
   {"/dev/events",0,0,0,events_read,invalid_write},
   {"/dev/fbsync",1,0,0,invalid_read,fbsync_write},
-  {"/dev/dispinfo",0,0,0,dispinfo_read,invalid_write},
+  {"/proc/dispinfo",0,0,0,dispinfo_read,invalid_write},
   {"/dev/fb",0,0,0,invalid_read,fb_write},
 };
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
