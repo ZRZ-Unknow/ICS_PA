81a9f7e3a76255224517095efe8b1cacf13cf04b
tracer-ics2019 <tracer@njuics.org>
2019-11-25 15:11:03 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  15:11:03 up 2 min,  1 user,  load average: 0.53, 0.24, 0.09 cb1b707035868ee4354082f79ae7f43ee47e4adf

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -6,7 +6,6 @@ int main() {
   assert(fp);
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
-  //printf("size is %d\n",size);
   assert(size == 5000);
   fseek(fp, 5*500, SEEK_SET);
   int i, n;
@@ -19,25 +18,16 @@ int main() {
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-  //fseek(fp,0,SEEK_END);printf("after insert size is %d\n",ftell(fp));
   //此时文件头部应写入了1001-1500，n为1000,移动到2500,文件为1001-1500,501-1000
   for (i = 500; i < 1000; i ++) {
-    //printf("i is %d\n",i);
-    //printf("curr pos is %d\n",ftell(fp));
+    printf("i is %d\n",i);
+    printf("curr pos is %d\n",ftell(fp));
     fscanf(fp, "%d", &n);
-    //printf("n is %d\n",n);
-    //printf("curr pos is %d\n",ftell(fp));
-    //printf("before second\n");
-    //fscanf(fp,"%d",&n);
-    //printf("n is %d\n",n);
-    //printf("curr pos is %d\n",ftell(fp));
     assert(n == i + 1);
   }
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
-    //printf("n is %d\n",n);
-    //printf("curr pos is %d\n",ftell(fp));
     assert(n == i + 1 + 1000);
   }
   fclose(fp);
