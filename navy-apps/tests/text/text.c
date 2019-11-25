043601421d6afbc75303fa0a06e0bd6aaabcfc77
tracer-ics2019 <tracer@njuics.org>
2019-11-25 15:12:42 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  15:12:42 up 4 min,  1 user,  load average: 0.66, 0.35, 0.15 52dbe135ea994c573e1b2d5c1ffb0b155f6c9fff

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 #include <assert.h>
-//printf调用fs_write,ramdiskwrite的.write
+
 int main() {
   FILE *fp = fopen("/share/texts/num", "r+");
   assert(fp);
   fseek(fp, 0, SEEK_END);
+
   long size = ftell(fp);
   assert(size == 5000);
   fseek(fp, 5*500, SEEK_SET);
+
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
+
   fseek(fp, 0, SEEK_SET);
-  //printf("now pos is %d\n",ftell(fp));
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-  //此时文件头部应写入了1001-1500，n为1000,移动到2500,文件为1001-1500,501-1000
+
   for (i = 500; i < 1000; i ++) {
-    printf("i is %d\n",i);
-    printf("curr pos is %d\n",ftell(fp));
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
+
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
+
   fclose(fp);
 
   printf("PASS!!!\n");
