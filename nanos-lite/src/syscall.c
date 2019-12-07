a7f12aa5bb466eefd834793a0a662124e6529eab
tracer-ics2019 <tracer@njuics.org>
2019-12-07 16:02:31 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:02:31 up  2:12,  1 user,  load average: 1.05, 0.71, 0.59 50156193efc2f52d887d6a5a0126219b6890dc4

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -22,15 +22,15 @@ _Context* do_syscall(_Context *c) {
   a[2]=c->GPR3;
   a[3]=c->GPR4;
   switch (a[0]) {
-    case SYS_yield:Log("sys_yield");_yield();c->GPRx=0;break;
-    case SYS_exit:Log("sys_exit");c->GPRx=-1;naive_uload(NULL,"/bin/init");break;//_halt(a[1]);break;
-    case SYS_brk:Log("sys_brk");c->GPRx=0;break;
-    case SYS_open:Log("sys_open");c->GPRx=fs_open((void*)a[1],a[2],a[3]);break;
-    case SYS_read:Log("read");c->GPRx=fs_read(a[1],(void*)a[2],a[3]);break;
-    case SYS_write:Log("sys_write");c->GPRx=fs_write(a[1],(void*)a[2],a[3]);break;
-    case SYS_close:Log("sys_close");c->GPRx=fs_close(a[1]);break;
-    case SYS_lseek:Log("sys_lseek");c->GPRx=fs_lseek(a[1],a[2],a[3]);break;
-    case SYS_execve:Log("sys_execve");c->GPRx=-1;naive_uload(NULL,(char*)a[1]);break;
+    case SYS_yield:_yield();c->GPRx=0;break;
+    case SYS_exit:c->GPRx=-1;naive_uload(NULL,"/bin/init");break;//_halt(a[1]);break;
+    case SYS_brk:c->GPRx=0;break;
+    case SYS_open:c->GPRx=fs_open((void*)a[1],a[2],a[3]);break;
+    case SYS_read:c->GPRx=fs_read(a[1],(void*)a[2],a[3]);break;
+    case SYS_write:c->GPRx=fs_write(a[1],(void*)a[2],a[3]);break;
+    case SYS_close:c->GPRx=fs_close(a[1]);break;
+    case SYS_lseek:c->GPRx=fs_lseek(a[1],a[2],a[3]);break;
+    case SYS_execve:c->GPRx=-1;naive_uload(NULL,(char*)a[1]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
