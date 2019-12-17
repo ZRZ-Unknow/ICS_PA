c634b389cc06f283917ce0579f4899a67233162a
tracer-ics2019 <tracer@njuics.org>
2019-12-17 22:49:59 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:49:59 up  2:46,  1 user,  load average: 0.62, 0.46, 0.24 6c7250e2709ccaec78790a2bfef22e13bbc9dcbd

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -2,13 +2,16 @@
 #include <x86.h>
 
 static _Context* (*user_handler)(_Event, _Context*) = NULL;
-
+extern void __am_get_cur_as(_Context *c); 
+extern void __am_switch(_Context *c);
 void __am_irq0();
 void __am_vecsys();
 void __am_vectrap();
 void __am_vecnull();
 
 _Context* __am_irq_handle(_Context *c) {
+  __am_get_cur_as(c);
+
   _Context *next = c;
   if (user_handler) {
     _Event ev = {0};
@@ -23,7 +26,7 @@ _Context* __am_irq_handle(_Context *c) {
       next = c;
     }
   }
-
+  __am_switch(next);
   return next;
 }
 
