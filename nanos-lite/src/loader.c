05985cc14c913e8e37769a4d9e553b9dbcb9810f
tracer-ics2019 <tracer@njuics.org>
2019-12-17 21:34:16 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:34:16 up  1:30,  1 user,  load average: 0.50, 0.52, 0.35 b2bb7e916eaddb58a2ebbd8a17946030a321e814

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,6 +11,12 @@
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();  
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+#define PTE_ADDR(pte)    ((uint32_t)(pte) & ~0xfff)
+#define OFF(va)          ((uint32_t)(va) & 0xfff)
+
+static inline uint32_t get_size(int32_t fsize,int32_t usize){
+  return fsize<usize ? fsize:usize;
+}
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   int fd=fs_open(filename,0,0);
@@ -24,8 +30,35 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     fs_read(fd,(void*)&phdr[i],elf.e_phentsize);
     if(phdr[i].p_type==PT_LOAD){
       fs_lseek(fd,phdr[i].p_offset,SEEK_SET);
-
-      //4.2第一次写的loader
+      void *vaddr = (void *)phdr[i].p_vaddr;
+      void *paddr=new_page(1);
+      int32_t fsize = phdr[i].p_filesz;
+      _map(&pcb->as, vaddr, paddr, 0);
+      uint32_t wsize = get_size(fsize, PTE_ADDR((uint32_t)vaddr + PGSIZE) - (uint32_t)vaddr);
+      fs_read(fd, (void *)(PTE_ADDR(paddr) | OFF(vaddr)), wsize);
+      fsize -= wsize;
+      vaddr += wsize;
+      for (; fsize > 0; fsize -= wsize, vaddr += wsize) {
+        assert(((uint32_t)vaddr & 0xfff) == 0);
+        paddr = new_page(1);
+        _map(&pcb->as, vaddr, paddr, 0);
+        wsize = get_size(fsize, PGSIZE);
+        fs_read(fd, paddr, wsize);
+      }
+      fsize = phdr[i].p_memsz - phdr[i].p_filesz;
+      if (((uint32_t)vaddr & 0xfff) != 0) {
+        wsize = get_size(fsize, PTE_ADDR((uint32_t)vaddr + PGSIZE) - (uint32_t)vaddr);
+        memset((void *)(PTE_ADDR(paddr) | OFF(vaddr)), 0, wsize);
+        fsize -= wsize;
+        vaddr += wsize;
+      }
+      for (wsize = PGSIZE;fsize > 0; fsize -= wsize, vaddr += wsize) {
+        assert(((uint32_t)vaddr & 0xfff) == 0);
+        paddr = new_page(1);
+        _map(&pcb->as, vaddr, paddr, 0);
+        memset(paddr, 0, wsize);
+      }
+      /*//4.2第一次写的loader
       void *va=(void*)phdr[i].p_paddr;
       void *pa;
       int32_t filesz=phdr[i].p_filesz;
@@ -35,7 +68,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
         fs_read(fd,pa,PGSIZE);
         va+=PGSIZE;
         filesz-=PGSIZE;
-      }
+      }*/
       //fs_read(fd,(void*)phdr[i].p_vaddr,phdr[i].p_filesz);
       //memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz),0,phdr[i].p_memsz-phdr[i].p_filesz);
     }
