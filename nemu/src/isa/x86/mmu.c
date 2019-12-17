66af623179779e04b5b3348c9524505cef5c0d49
tracer-ics2019 <tracer@njuics.org>
2019-12-17 22:56:55 +0800

    >  compile 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:56:55 up  2:52,  1 user,  load average: 0.67, 0.44, 0.30 113cf3af714a3939e45b696aa5b86369aa475505

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -6,8 +6,8 @@
 #define PTX(va)          (((uint32_t)(va) >> PTXSHFT) & 0x3ff)
 #define OFF(va)          ((uint32_t)(va) & 0xfff)
 #define PTE_ADDR(pte)    ((uint32_t)(pte) & ~0xfff)
-
-paddr_t page_translate(vaddr_t ad){
+#define PTE_P          0x001 
+/*paddr_t page_translate(vaddr_t ad){
   PDE pde;
   PTE pte;  
   pde.val=paddr_read(cpu.cr3.val+4*PDX(ad),4);
@@ -51,5 +51,59 @@ void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
   }
   else{
     paddr_write(addr, data,len);
+  }
+}*/
+
+inline paddr_t page_translate(vaddr_t va) {
+  paddr_t ptab = paddr_read(cpu.cr3.val + sizeof(PDE) * PDX(va), sizeof(PDE));
+  if (!(ptab & PTE_P)) {
+    printf("ERROR:page_translate(): page table doesn't exists!\n");
+    assert(0); 
+  }
+
+  paddr_t pg = paddr_read(PTE_ADDR(ptab) + sizeof(PTE) * PTX(va), sizeof(PTE));
+  if (!(pg & PTE_P)) {
+    printf("ERROR:page_translate(): page doesn't exists!\n");
+    assert(0); 
+  }
+
+  return (PTE_ADDR(pg) | OFF(va));
+}
+
+uint32_t isa_vaddr_read(vaddr_t addr, int len) {
+  if (cpu.cr0.paging) { 
+    if (PTE_ADDR(addr) != PTE_ADDR(addr + len - 1)) { 
+      uint8_t byte[4];
+      for (int i = 0; i < len; i++)
+        byte[i] = isa_vaddr_read(addr + i, 1);
+      if (len == 2)
+        return *(uint16_t *)byte;
+      else
+        return *(uint32_t *)byte;
+    } else {
+      paddr_t paddr = page_translate(addr);
+      return paddr_read(paddr, len);
+    }
+  } else {
+    return paddr_read(addr, len);
+  }
+}
+
+void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
+  if (cpu.cr0.paging) { 
+    if (PTE_ADDR(addr) != PTE_ADDR(addr + len - 1)) { 
+      uint8_t byte[4];
+      if (len == 2)
+        *(uint16_t *)byte = data;
+      else
+        *(uint32_t *)byte = data;
+      for (int i = 0; i < len; i++)
+        isa_vaddr_write(addr + i, byte[i], 1);
+    } else {  
+      paddr_t paddr = page_translate(addr);
+      paddr_write(paddr, data, len);
+    }
+  } else {  
+    paddr_write(addr, data, len);
   }
 }
