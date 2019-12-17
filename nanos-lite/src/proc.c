c154225e915ea101335c6146f3a885d2561df678
tracer-ics2019 <tracer@njuics.org>
2019-12-18 01:13:41 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:13:41 up  5:09,  1 user,  load average: 0.38, 0.27, 0.26 10c6012f5107054ee3e029b22d0c1c8db5f511e9

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,7 @@ void init_proc() {
   //context_kload(&pcb[0],(void*)hello_fun);
   //context_uload(&pcb[0],"/bin/hello");
   context_uload(&pcb[0],"/bin/pal");
-  switch_boot_pcb();
+  //switch_boot_pcb();
   Log("Initializing processes...");
 
   // load program here
