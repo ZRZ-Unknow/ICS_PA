d4938f7017d34b0ea06d9dce516a463d436a99cd
tracer-ics2019 <tracer@njuics.org>
2019-12-19 01:15:05 +0800

    >  run 181220076 zrz Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:15:05 up 42 min,  1 user,  load average: 0.66, 0.50, 0.38 e455660f1593a4a6d87e058b58b2bfb8cf6dd426

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   //context_kload(&pcb[0],(void*)hello_fun);
   //context_uload(&pcb[0],"/bin/hello");
-  context_uload(&pcb[0],"/bin/hello");
+  context_uload(&pcb[0],"/bin/events");
   context_uload(&pcb[1],"/bin/pal");
   context_uload(&pcb[2],"/bin/pal");
   context_uload(&pcb[3],"/bin/pal");
